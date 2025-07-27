baa13248c04117b665c84107f4e36899cce10c2d
heitorrosa <heitorolivrosa@gmail.com>
2025-07-27 17:21:36 -0300

    feat: added historical dividends yields and an average dividend yield over 5 years (luis barsi strategy)

--- src/scraper.py
+++ src/scraper.py
@@ -5,12 +5,13 @@ import csv
 import time
 import concurrent.futures
 
+import selenium
+import mysql.connector
+
+
 from time import sleep
 from datetime import datetime
 from selenium import webdriver
-
-import selenium
-import mysql.connector
 
 # Configs
 start_time = time.time()
@@ -26,59 +27,36 @@ dateScrape = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 csvUrl = 'https://statusinvest.com.br/category/AdvancedSearchResultExport?search=%7B%22Sector%22%3A%22%22%2C%22SubSector%22%3A%22%22%2C%22Segment%22%3A%22%22%2C%22my_range%22%3A%22-20%3B100%22%2C%22forecast%22%3A%7B%22upsidedownside%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22estimatesnumber%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22revisedup%22%3Atrue%2C%22reviseddown%22%3Atrue%2C%22consensus%22%3A%5B%5D%7D%2C%22dy%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_l%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22peg_ratio%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_vp%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_ativo%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22margembruta%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22margemebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22margemliquida%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_ebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22ev_ebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22dividaliquidaebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22dividaliquidapatrimonioliquido%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_sr%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_capitalgiro%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_ativocirculante%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22roe%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22roic%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22roa%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22liquidezcorrente%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22pl_ativo%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22passivo_ativo%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22giroativos%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22receitas_cagr5%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22lucros_cagr5%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22liquidezmediadiaria%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22vpa%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22lpa%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22valormercado%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%7D&CategoryType=1'
 
+csvUrlTest = 'https://statusinvest.com.br/category/AdvancedSearchResultExport?search=%7B%22Sector%22%3A%2210%22%2C%22SubSector%22%3A%2243%22%2C%22Segment%22%3A%2284%22%2C%22my_range%22%3A%22-20%3B100%22%2C%22forecast%22%3A%7B%22upsidedownside%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22estimatesnumber%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22revisedup%22%3Atrue%2C%22reviseddown%22%3Atrue%2C%22consensus%22%3A%5B%5D%7D%2C%22dy%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_l%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22peg_ratio%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_vp%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_ativo%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22margembruta%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22margemebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22margemliquida%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_ebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22ev_ebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22dividaliquidaebit%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22dividaliquidapatrimonioliquido%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_sr%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_capitalgiro%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22p_ativocirculante%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22roe%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22roic%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22roa%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22liquidezcorrente%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22pl_ativo%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22passivo_ativo%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22giroativos%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22receitas_cagr5%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22lucros_cagr5%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22liquidezmediadiaria%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22vpa%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22lpa%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%2C%22valormercado%22%3A%7B%22Item1%22%3Anull%2C%22Item2%22%3Anull%7D%7D&CategoryType=1'
+
 #
 # Setup Selenium WebDriver
 #
 
 options = webdriver.ChromeOptions()
+
 prefs = {
-    "profile.managed_default_content_settings.images": 2,
     "download.default_directory": download_folder,
     "download.prompt_for_download": False,
     "download.directory_upgrade": True,
-    "safebrowsing.enabled": True
+    "profile.managed_default_content_settings.images": 2,
+    "profile.default_content_setting_values.javascript": 2,
+    "safebrowsing.enabled": False
 }
 
 options.add_experimental_option("prefs", prefs)
 options.add_argument('--headless=new')
-options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+# options.add_argument('user-agent=VALID_USER_AGENT')
 
-options.add_argument('--blink-settings=imagesEnabled=false')
-options.add_argument('--disable-infobars')
-options.add_argument('--disable-notifications')
-options.add_argument('--disable-popup-blocking')
-options.add_argument('--disable-background-networking')
-options.add_argument('--disable-sync')
-options.add_argument('--disable-default-apps')
-options.add_argument('--disable-translate')
-options.add_argument('--disable-features=TranslateUI')
-
-options.add_argument('--disable-background-timer-throttling')
-options.add_argument('--disable-backgrounding-occluded-windows')
-options.add_argument('--disable-client-side-phishing-detection')
-options.add_argument('--disable-component-update')
-options.add_argument('--disable-hang-monitor')
-options.add_argument('--disable-ipc-flooding-protection')
-options.add_argument('--disable-prompt-on-repost')
-options.add_argument('--disable-renderer-backgrounding')
-options.add_argument('--metrics-recording-only')
-options.add_argument('--no-first-run')
-options.add_argument('--safebrowsing-disable-auto-update')
-options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument('--disable-logging')
-options.add_argument('--disable-permissions-api')
-options.add_argument('--disable-translate')
-options.add_argument('--mute-audio')
-
 options.add_argument('--log-level=3')
 options.add_argument('--silent')
 
 options.add_extension(os.path.join(script_dir, 'data/ublock.crx'))
 
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36")
-
 driver = webdriver.Chrome(options=options)
 
 
@@ -96,63 +74,39 @@ def formatNumber(number):
         number = float(number.replace('B', '')) * 1000000000
     else:
         number = float(number)
-    return number
+    return round(number, 2)
 
 def scrape_stock(row):
     options = webdriver.ChromeOptions()
+
     prefs = {
-        "profile.managed_default_content_settings.images": 2,
         "download.default_directory": download_folder,
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
-        "safebrowsing.enabled": True
+        "profile.managed_default_content_settings.images": 2,
+        "profile.default_content_setting_values.javascript": 2,
+        "safebrowsing.enabled": False
     }
 
     options.add_experimental_option("prefs", prefs)
     options.add_argument('--headless=new')
-    options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-
-    options.add_argument('--blink-settings=imagesEnabled=false')
-    options.add_argument('--disable-infobars')
-    options.add_argument('--disable-notifications')
-    options.add_argument('--disable-popup-blocking')
-    options.add_argument('--disable-background-networking')
-    options.add_argument('--disable-sync')
-    options.add_argument('--disable-default-apps')
-    options.add_argument('--disable-translate')
-    options.add_argument('--disable-features=TranslateUI')
-
-    options.add_argument('--disable-background-timer-throttling')
-    options.add_argument('--disable-backgrounding-occluded-windows')
-    options.add_argument('--disable-client-side-phishing-detection')
-    options.add_argument('--disable-component-update')
-    options.add_argument('--disable-hang-monitor')
-    options.add_argument('--disable-ipc-flooding-protection')
-    options.add_argument('--disable-prompt-on-repost')
-    options.add_argument('--disable-renderer-backgrounding')
-    options.add_argument('--metrics-recording-only')
-    options.add_argument('--no-first-run')
-    options.add_argument('--safebrowsing-disable-auto-update')
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument('--disable-logging')
-    options.add_argument('--disable-permissions-api')
-    options.add_argument('--disable-translate')
-    options.add_argument('--mute-audio')
-
-    options.add_argument('--log-level=3')
-    options.add_argument('--silent')
+    options.add_argument('user-agent=VALID_USER_AGENT')
 
     options.add_extension(os.path.join(script_dir, 'data/ublock.crx'))
 
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36")
+    options.add_argument('--disable-logging')
+    options.add_argument('--log-level=3')
+    options.add_argument('--silent')
+
+    # options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36")
     driver = webdriver.Chrome(options=options)
 
     try:
         TICKER = row.get('TICKER', '')
         PRECO = row.get('PRECO', '') or row.get('PREÇO', '')
-        DY = row.get('DY', '')
+        DY_12_MESES = row.get('DY', '')
         P_L = row.get('P/L', '')
         P_VP = row.get('P/VP', '')
         P_ATIVOS = row.get('P/ATIVOS', '')
@@ -263,10 +217,77 @@ def scrape_stock(row):
         except Exception:
             rent1dia = rent5dias = rent1mes = rent6mes = rentYTD = rent1ano = rent5anos = rentTotal = rentMedia5anos = ''
 
+        # Get historical dividends yields
+        try:
+            def parseJSON(data):
+                pastDividendos = ''
+
+                for dividendos in data['assetEarningsYearlyModels']:
+                    dividendos = f'{pastDividendos} \n {dividendos}'
+                    pastDividendos = dividendos
+
+                return dividendos
+    
+            driver.get(f'https://statusinvest.com.br/acao/companytickerprovents?companyName=weg&ticker={TICKER}&chartProventsType=2')
+            jsonData = (driver.find_element('xpath', '/html/body/pre').text)
+            jsonData = json.loads(jsonData)
+            dividendList = parseJSON(jsonData)
+
+            for i in dividendList:
+                dividendList = dividendList.replace('{', '')
+                dividendList = dividendList.replace('}', '')
+                dividendList = dividendList.replace('"', '')
+                dividendList = dividendList.replace('[', '')
+                dividendList = dividendList.replace(']', '')
+                dividendList = dividendList.replace(',', '')
+                dividendList = dividendList.replace(':', '')
+                dividendList = dividendList.replace('\'', '')
+                dividendList = dividendList.replace('  ', ' ')
+
+                dividendList = dividendList.replace('rank', '')
+                dividendList = dividendList.replace('value', '')
+
+            dividendList = dividendList.replace(' ', '\n')
+            dividendList = dividendList.split('\n')
+            dividendList = [item for item in dividendList if item]
+            dividendList = [item for item in dividendList if not (item.isdigit() and len(item) == 4)]
+
+            dividendList = dividendList[::-1]
+
+            for i in dividendList:
+                if i == ' ' or i == '':
+                    dividendList.remove(i)
+
+            dividendList = [float(item) for item in dividendList]
+
+            try:
+                dy_medio_5anos = (((dividendList[0] + dividendList[1] + dividendList[2] + dividendList[3] + dividendList[4])/5) / 36.19) * 100
+                dy_medio_5anos = round(dy_medio_5anos, 2)
+            except Exception:
+                dy_medio_5anos = ''
+
+            dy = []
+
+            for i, value in enumerate(dividendList[1:5]):
+                try:
+                    dividendyield = (value / 36.19) * 100
+                    dividendyield = round(dividendyield, 2)
+                except Exception:
+                    dividendyield = ''
+
+                dy.append(dividendyield)
+        except Exception as e:
+            dy = []
+            dy_medio_5anos = ''
+
+        # Get historical liquidity
+
+
+        # Create stock dictionary
         stock = {
             'ticker': TICKER,
             'preco': PRECO,
-            'dy': DY,
+            'dy_12_meses': DY_12_MESES,
             'p_l': P_L,
             'p_vp': P_VP,
             'p_ativos': P_ATIVOS,
@@ -294,9 +315,6 @@ def scrape_stock(row):
             'lpa': LPA,
             'peg_ratio': PEG_RATIO,
             'valor_de_mercado': VALOR_DE_MERCADO,
-            'setor': setor,
-            'subsetor': subsetor,
-            'segmento': segmento,
             'tag_along': tagAlong,
             'rent_media_5_anos': rentMedia5anos,
             'rent_1_dia': rent1dia,
@@ -307,8 +325,17 @@ def scrape_stock(row):
             'rent_1_ano': rent1ano,
             'rent_5_anos': rent5anos,
             'rent_total': rentTotal,
+            'dy_medio_5anos': dy_medio_5anos,
+            'dy_1ano_atras': dy[0],
+            'dy_2anos_atras': dy[1],
+            'dy_3anos_atras': dy[2],
+            'dy_4anos_atras': dy[3],
+            'setor': setor,
+            'subsetor': subsetor,
+            'segmento': segmento,
             'scrape_time': dateScrape
         }
+
         return stock
     except Exception as e:
         return None
@@ -329,7 +356,7 @@ if os.path.isfile(download_folder + '/stocks_data.jsonl'):
     os.remove(download_folder + '/stocks_data.jsonl')
 
 # Download the CSV file
-driver.get(csvUrl)
+driver.get(csvUrlTest)
 sleep(3)
 driver.quit()
 
@@ -365,13 +392,14 @@ mysql_config = {
     'database': 'b3'
 }
 fields = [
-    'ticker', 'preco', 'dy', 'p_l', 'p_vp', 'p_ativos', 'margem_bruta', 'margem_ebit', 'marg_liquida',
+    'ticker', 'preco', 'dy_12_meses', 'p_l', 'p_vp', 'p_ativos', 'margem_bruta', 'margem_ebit', 'marg_liquida',
     'p_ebit', 'ev_ebit', 'divida_liquida_ebit', 'div_liq_patri', 'psr', 'p_cap_giro', 'p_at_cir_liq',
     'liq_corrente', 'roe', 'roa', 'roic', 'patrimonio_ativos', 'passivos_ativos', 'giro_ativos',
     'cagr_receitas_5_anos', 'cagr_lucros_5_anos', 'liquidez_media_diaria', 'vpa', 'lpa', 'peg_ratio',
-    'valor_de_mercado', 'setor', 'subsetor', 'segmento', 'tag_along', 'rent_media_5_anos', 'rent_1_dia',
+    'valor_de_mercado', 'tag_along', 'rent_media_5_anos', 'rent_1_dia',
     'rent_5_dias', 'rent_1_mes', 'rent_6_meses', 'rent_ytd', 'rent_1_ano', 'rent_5_anos', 'rent_total',
-    'scrape_time'
+    'dy_medio_5anos', 'dy_1ano_atras', 'dy_2anos_atras', 'dy_3anos_atras', 'dy_4anos_atras',
+    'setor', 'subsetor', 'segmento', 'scrape_time'
 ]
 
 conn = mysql.connector.connect(**mysql_config)
@@ -381,7 +409,7 @@ CREATE TABLE IF NOT EXISTS stocks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     ticker VARCHAR(16),
     preco FLOAT,
-    dy FLOAT,
+    dy_12_meses FLOAT,
     p_l FLOAT,
     p_vp FLOAT,
     p_ativos FLOAT,
@@ -409,9 +437,6 @@ CREATE TABLE IF NOT EXISTS stocks (
     lpa FLOAT,
     peg_ratio FLOAT,
     valor_de_mercado FLOAT,
-    setor VARCHAR(64),
-    subsetor VARCHAR(64),
-    segmento VARCHAR(64),
     tag_along VARCHAR(16),
     rent_media_5_anos FLOAT,
     rent_1_dia FLOAT,
@@ -422,6 +447,14 @@ CREATE TABLE IF NOT EXISTS stocks (
     rent_1_ano FLOAT,
     rent_5_anos FLOAT,
     rent_total FLOAT,
+    dy_medio_5anos FLOAT,
+    dy_1ano_atras FLOAT,
+    dy_2anos_atras FLOAT,
+    dy_3anos_atras FLOAT,
+    dy_4anos_atras FLOAT,
+    setor VARCHAR(64),
+    subsetor VARCHAR(64),
+    segmento VARCHAR(64),
     scrape_time DATETIME
 )
 """)
